a401ea01349146d964c2501112c4004d63b4a1cb
matheusmorais1201-ai <mtorres15@byupathway.edu>
2026-09-05 19:08:37 -0300

    date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,14 +7,5 @@ const year = document.querySelector("#currentyear");
 
 const today = new Date();
 
-const date = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "short"
-}).format(today);
-
-const time = new Intl.DateTimeFormat("en-US", {
-    timeStyle: "medium"
-}).format(today);
-
-short.innerHTML = `Last Modification: <span class="highlight">${date} ${time}</span>`;
-
-year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+short.textContent = document.lastModified;
+year.textContent = today.getFullYear();
